9165ea2e5f741e8df5e2e57c493425a958607415
Omkar Patole <omkarpatole1799@gmail.com>
2023-07-20 23:08:01 +0530

    snake game

---
+++ Script.js
@@ -0,0 +1,169 @@
+"use strict";
+const board = document.getElementById("board");
+let snakeArr;
+let snakeFood;
+let snakeMovements;
+
+function newGame() {
+  snakeArr = [
+    {
+      x: 10,
+      y: 10,
+    },
+  ];
+  snakeFood = [
+    { x: 10, y: 14 },
+    { x: 15, y: 5 },
+  ];
+  snakeMovements = { x: 0, y: 0 };
+}
+newGame();
+setInterval(() => {
+  gameEngine();
+}, 150);
+
+// check collision
+function isColloide(snape) {
+  // self collision
+  for (let i = 1; i < snape.length; i++) {
+    if (snape[i].x === snape[0].x && snape[i].y === snape[0].y) {
+      newGame();
+      return true;
+    }
+  }
+  // wall collision
+  if (
+    snape[0].x >= 20 ||
+    snape[0].y >= 20 ||
+    snape[0].x <= 0 ||
+    snape[0].y <= 0
+  ) {
+    newGame();
+    return true;
+  }
+
+  return false;
+}
+// check if food is eaten
+function checkFoodEaten(snape) {
+  let condition1 =
+    snape[0].x === snakeFood[0].x && snape[0].y === snakeFood[0].y;
+  let condition2 =
+    snape[0].x === snakeFood[1].x && snape[0].y === snakeFood[1].y;
+  if (condition1) {
+    return { first: true, second: false };
+  }
+  if (condition2) {
+    return { first: false, second: true };
+  }
+  return { first: false, second: false };
+}
+
+// random number generate function
+function generateRandomNumber() {
+  return Math.floor(Math.random() * 19) + 1;
+}
+
+// generate food
+function generateFood() {
+  let a, b;
+  a = generateRandomNumber();
+  b = generateRandomNumber();
+  snakeFood[0] = { x: a, y: b };
+}
+
+function generateFood2() {
+  let a, b;
+  a = generateRandomNumber();
+  b = generateRandomNumber();
+  snakeFood[1] = { x: a, y: b };
+}
+
+function gameEngine() {
+  if (isColloide(snakeArr)) {
+    return;
+  }
+  // generate food if eaten
+  let check = checkFoodEaten(snakeArr);
+  if (check.first === true && check.second === false) {
+    snakeArr.unshift({
+      x: snakeArr[0].x + snakeMovements.x,
+      y: snakeArr[0].y + snakeMovements.y,
+    });
+    generateFood();
+  }
+
+  if (check.second === true && check.first === false) {
+    snakeArr.unshift({
+      x: snakeArr[0].x + snakeMovements.x,
+      y: snakeArr[0].y + snakeMovements.y,
+    });
+    generateFood2();
+  }
+
+  for (let i = snakeArr.length - 2; i >= 0; i--) {
+    snakeArr[i + 1] = { ...snakeArr[i] };
+  }
+
+  // snake movements
+  snakeArr[0].x += snakeMovements.x;
+  snakeArr[0].y += snakeMovements.y;
+
+  // display snake
+  board.innerHTML = "";
+
+  snakeArr.forEach((element, i) => {
+    let snakeElement = document.createElement("div");
+    snakeElement.style.gridColumnStart = element.x;
+    snakeElement.style.gridRowStart = element.y;
+    if (i === 0) {
+      snakeElement.classList.add("snake-head");
+    } else {
+      snakeElement.classList.add("snake-body");
+    }
+    board.appendChild(snakeElement);
+  });
+
+  // disply food 1
+  let foodElement = document.createElement("div");
+  foodElement.style.gridColumnStart = snakeFood[0].x;
+  foodElement.style.gridRowStart = snakeFood[0].y;
+  foodElement.classList.add("snake-food");
+  board.appendChild(foodElement);
+
+  // display food 2
+  let foodElement2 = document.createElement("div");
+  foodElement2.style.gridColumnStart = snakeFood[1].x;
+  foodElement2.style.gridRowStart = snakeFood[1].y;
+  foodElement2.classList.add("snake-food");
+  board.appendChild(foodElement2);
+}
+
+// controls
+window.addEventListener("keydown", (e) => {
+  switch (e.key) {
+    case "ArrowUp":
+      snakeMovements.x = 0;
+      snakeMovements.y = -1;
+
+      break;
+
+    case "ArrowDown":
+      snakeMovements.x = 0;
+      snakeMovements.y = 1;
+      break;
+
+    case "ArrowLeft":
+      snakeMovements.x = -1;
+      snakeMovements.y = 0;
+      break;
+
+    case "ArrowRight":
+      snakeMovements.x = 1;
+      snakeMovements.y = 0;
+      break;
+
+    default:
+      break;
+  }
+});
